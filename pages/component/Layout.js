22e9dc57794c7431a132941ce42c5b8170ae847b
sowmiyaEE <sowmiya.skm@gmail.com>
2021-07-20 19:23:53 +0530

    Create Layout.js

---
+++ pages/component/Layout.js
@@ -0,0 +1,6 @@
+import Head from "next/Head";
+const Layout=() => (
+return(<Head>
+<script src="https://identity.netlify.com/v1/netlify-identity-widget.js"></script>
+</Head>););
+export default Layout;
