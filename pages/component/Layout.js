b6461d5e374a601fa3baccbb219b48110fbbf257
sowmiyaEE <sowmiya.skm@gmail.com>
2021-07-20 23:15:01 +0530

    Update Layout.js

--- pages/component/Layout.js
+++ pages/component/Layout.js
@@ -1,6 +1,4 @@
 //import Head from "next/Head";
 const Layout=() => (
-return(<Head>
-<script src="https://identity.netlify.com/v1/netlify-identity-widget.js"></script>
-</Head>););
+return(<div> </div>);
 export default Layout;
