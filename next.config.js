be6de4164daeb09449968c1d18f29c9d46f42c2d
sowmiyaEE <sowmiya.skm@gmail.com>
2021-08-02 17:38:45 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,13 +1,6 @@
 
-module.exports = {
-  // Target must be serverless
-  webpack5: false,
-  target: "serverless",
-  webpack: function (config) {
-    config.module.rules.push({
-      test: /\.md$/,
-      use: 'raw-loader',
-    })
-    return config
-  }
-};
+const withCSS = require('@zeit/next-css')
+modules.exports = withCSS({
+  target: 'serverless',
+   webpack5: false,
+})
