e5a060751fdb6fa9f13094a3e5153d9b0e31e193
sowmiyaEE <sowmiya.skm@gmail.com>
2021-08-02 19:06:59 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -2,4 +2,11 @@
 module.exports = {
   // Target must be serverless
   target: "serverless",
+   webpack: function (config) {
+    config.module.rules.push({
+      test: /\.md$/,
+      use: "raw-loader",
+    })
+    return config
+  }
  };
