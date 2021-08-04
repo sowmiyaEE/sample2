79259b2d1343e8f61a5ba81bfa4e2769ea5d5c17
sowmiyaEE <sowmiya.skm@gmail.com>
2021-08-04 09:14:32 +0530

    Update [...Showpaths].js

--- pages/courselist/[...Showpaths].js
+++ pages/courselist/[...Showpaths].js
@@ -97,7 +97,7 @@ console.log('path',pathname);
 if(pathname.includes('/true')){
 const s=pathname.replace('/true','.md');
 const file=import(`../../course/${s}`);
-const rfi=matter(file.default.toString());
+const rfi=matter(file.toString());
 console.log("check matter",rfi);
 return({props:{slugs:[rfi.content,rfi.data.title,rfi.data.description],pare:s,directory:true}});
 }
@@ -113,7 +113,7 @@ const rf0=files.map(filename=>
 {if(filename.includes(".md")){
 const file=import(`../../course/${pathname}${filename}`);
 
-rf2=matter(file.default.toString());
+rf2=matter(file.toString());
 filename=rf2.data.title+"-"+rf2.data.description+"...";
 console.log('files:data:<',rf2.data,'title',rf2.data.title);
 } 
